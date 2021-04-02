0d8dc169a4701a9ee52f80bf81e05bf6a024a2b4
David Beauchesne <david.beauchesne@uqar.ca>
2021-04-02 12:27:58 -0400

    first version of new conceptual figure

---
+++ Code/Figures/FR2-DynamicsCode.R
@@ -0,0 +1,221 @@
+library(deSolve)
+lwdD <- .75
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Omnivory
+#
+# Disturbances & dynamics
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# load('./Data/ParamInit/omParam.RData')
+# omParam <- as.data.frame(omParam)
+# uid = 88
+
+# # initial state
+# init <- c(x = omParam$x[uid],
+#           y = omParam$y[uid],
+#           z = omParam$z[uid])
+# initial state
+init <- c(x = 497.6478,
+          y = 259.8222,
+          z = 155.5680)
+
+# times
+times <- seq(0, 50, by = 1)
+
+deriv <- function(t, state, pars) {
+  with(as.list(c(state, pars)), {
+    dx <- x * (r - alpha*x - beta*y - gamma*z)
+    dy <- y * (-m_y + mu*beta*x - delta*z)
+    dz <- z * (-m_z + nu*gamma*x + omega*delta*y)
+    return(list(c(x = dx, y = dy, z = dz)))
+  })
+}
+
+# # Parameters
+# p <- c(r = 1.02,
+#    alpha = 0.0009,
+#     beta = omParam$beta[uid],
+#    delta = omParam$delta[uid],
+#    gamma = omParam$gamma[uid],
+#       mu = .5,
+#       nu = .5,
+#    omega = .5,
+#      m_y = omParam$m_y[uid],
+#      m_z = omParam$m_z[uid])
+# Parameters
+p <- c(r = 1.0200000000,
+   alpha = 0.0009000000,
+    beta = 0.0017214390,
+   delta = 0.0026447249,
+   gamma = 0.0003540844,
+      mu = 0.5000000000,
+      nu = 0.5000000000,
+   omega = 0.5000000000,
+     m_y = 0.0169005008,
+     m_z = 0.4316837251)
+
+plotOmnivory <- function() {
+  # plot0(x = c(-3.5,60), y = c(-60,625))
+  plot0(x = c(-3.5,60), y = c(-60,1000))
+  arrows(-2, 0, 59, 0, length = .05, code = 2, xpd = TRUE)
+  arrows(-2, 0, -2, 600, length = .05, code = 2, xpd = TRUE)
+  lines(x = c(5,5), y = c(-25,600), lty = 2, col = '#00000088')
+  lines(x = c(5,5), y = c(-25,0), col = '#000000')
+  text(x = 56, y = -15, adj = c(1,1), 'Time', cex = .75)
+  text(x = -3, y = 275, adj = c(.5,0), TeX('Abundance ($\\textit{a}$)'), cex = .75, srt = 90)
+  text(x = 6, y = -42, adj = c(0,1), 'Stressors', cex = .55)
+  for(i in 1:3) lines(x = c(0,5), y = rep(init[i], 2), col = '#000000', lwd = .75)
+  arrows(6, -25, 10, -25, length = .025, code = 2, xpd = TRUE)
+  pchImage(x = 1.5, 110, obj = bew, cex.x = .8, cex.y = .6, col = colsPos[3]) # Beluga
+  pchImage(x = 1.5, 220, obj = cod, cex.x = .7, cex.y = .5, col = colsPos[3]) # Cod
+  pchImage(x = 1.5, 460, obj = cap, cex.x = .5, cex.y = .5, col = colsPos[3]) # Capelin
+}
+
+# Disturbances 1
+p1 <- p
+p1['r'] <- p1['r']*.8
+# Solve system of equations
+res1 <- ode(init, times, deriv, p1)
+
+# Disturbances 2
+p2 <- p
+p2['m_y'] <- p2['m_y']*1.15
+# Solve system of equations
+res2 <- ode(init, times, deriv, p2)
+
+# # Disturbances 3
+# p3 <- p
+# p3['gamma'] <- p3['gamma']*.75
+# p3['delta'] <- p3['delta']*.8
+# # Solve system of equations
+# res3 <- ode(init, times, deriv, p3)
+
+# Disturbances 3
+p3 <- p
+p3['gamma'] <- p3['gamma']*.75
+# Solve system of equations
+res3 <- ode(init, times, deriv, p3)
+
+# Disturbances 3
+p4 <- p
+p4['delta'] <- p4['delta']*.8
+# Solve system of equations
+res4 <- ode(init, times, deriv, p4)
+
+
+# Disturbances 5
+p5 <- p
+p5['r'] <- p5['r']*.8
+p5['m_y'] <- p5['m_y']*1.15
+p5['gamma'] <- p5['gamma']*.75
+p5['delta'] <- p5['delta']*.8
+# Solve system of equations
+res5 <- ode(init, times, deriv, p5)
+
+# # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
+# # Measure trophic sensitivity, amplification and variance
+# Trophic sensitivity
+# resTS1 <- (res1[50, 2:4] - res1[1, 2:4])/res1[1,2:4]
+# resTS2 <- (res2[50, 2:4] - res2[1, 2:4])/res2[1,2:4]
+# resTS3 <- (res3[50, 2:4] - res3[1, 2:4])/res3[1,2:4]
+# resTS4 <- (res4[50, 2:4] - res4[1, 2:4])/res4[1,2:4]
+#
+# # Trophic amplification
+# expTS <- resTS4/3
+# resTA <- (resTS1-expTS) + (resTS2-expTS) + (resTS3-expTS)
+#
+# # Trophic amplification
+# resTV <- (resTS1-expTS)^2 + (resTS2-expTS)^2 + (resTS3-expTS)^2
+# # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Apparent competition
+#
+# Disturbances & dynamics
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+load('./Data/ParamInit/apParam.RData')
+load('./Data/ParamImpact/apImpact.RData')
+
+# i = 50
+# ParamInit = apParam[i,]
+# equilibrium = apEq
+# nParam <- length(ParamInit)
+# nmParam <- names(ParamInit)
+# lsInit <- split(as.matrix(ParamInit), nmParam)
+# InitAbundances <- do.call(equilibrium, lsInit)
+#
+# apParam <- as.data.frame(apParam)
+# uid = 88
+#
+# # initial state
+# init <- c(x = apParam$x[uid],
+#           y = apParam$y[uid],
+#           z = apParam$z[uid])
+
+# initial state
+apInit <- c(x = 388.2725,
+          y = 457.4762,
+          z = 1153.5065)
+
+# times
+times <- seq(0, 50, by = 1)
+
+deriv <- function(t, state, pars) {
+  with(as.list(c(state, pars)), {
+    dx <- x * (r_x - alpha_x*x - gamma*z)
+    dy <- y * (r_y - alpha_y*y - delta*z)
+    dz <- z * (-m_z + nu*gamma*x + omega*delta*y)
+    return(list(c(x = dx, y = dy, z = dz)))
+  })
+}
+
+
+# Parameters
+p <- c(alpha_x = 0.0010000000,
+       alpha_y = 0.0010000000,
+         delta = 0.0004703257,
+         gamma = 0.0005303200,
+           m_z = 0.2505357424,
+            nu = 0.5000000000,
+         omega = 0.5000000000,
+           r_x = 0.9000000000,
+           r_y = 1.100000000)
+
+
+# Plot function
+plotApparent <- function() {
+  # plot0(x = c(-3.5,60), y = c(-60,625))
+  plot0(x = c(-3.5,60), y = c(-60,2000))
+  arrows(-2, 0, 59, 0, length = .05, code = 2, xpd = TRUE)
+  arrows(-2, 0, -2, 1500, length = .05, code = 2, xpd = TRUE)
+  lines(x = c(5,5), y = c(-25,1500), lty = 2, col = '#00000088')
+  lines(x = c(5,5), y = c(-25,0), col = '#000000')
+  text(x = 56, y = -15, adj = c(1,1), 'Time', cex = .75)
+  text(x = -3, y = 700, adj = c(.5,0), TeX('Abundance ($\\textit{a}$)'), cex = .75, srt = 90)
+  text(x = 6, y = -42, adj = c(0,1), 'Stressors', cex = .55)
+  for(i in 1:3) lines(x = c(0,5), y = rep(apInit[i], 2), col = '#000000', lwd = .75)
+  arrows(6, -25, 10, -25, length = .025, code = 2, xpd = TRUE)
+  pchImage(x = 1.5, 550, obj = cop, cex.x = .5, cex.y = .6, col = colsPos[4]) # Copepods
+  pchImage(x = 1.5, 290, obj = kri, cex.x = .5, cex.y = .5, col = colsPos[4]) # Krill
+  pchImage(x = 1.5, 1080, obj = cap, cex.x = .5, cex.y = .5, col = colsPos[4]) # Capelin
+}
+
+# Unitary 1
+p1 <- p
+p1['omega'] <- p1['omega']*.8
+# Solve system of equations
+ap1 <- ode(apInit, times, deriv, p1)
+
+# Unitary 2
+p2 <- p
+p2['m_z'] <- p2['m_z']*.8
+# Solve system of equations
+ap2 <- ode(apInit, times, deriv, p2)
+
+# Unitary 1
+p3 <- p
+p3['m_z'] <- p3['m_z']*.8
+p3['omega'] <- p3['omega']*.8
+# Solve system of equations
+ap3 <- ode(apInit, times, deriv, p3)
---
+++ Code/Figures/FR2-Pathways.R
@@ -0,0 +1,426 @@
+# source('./Code/Figures/FR2-Pathways.R')
+source('./Code/0-Param.R')
+
+# ----------------------------------------------------------------------
+# Species
+# ----------------------------------------------------------------------
+# Humpback whale - Megaptera novaeangliae
+# by Chris huh
+# http://phylopic.org/image/ce70490a-79a5-47fc-afb9-834e45803ab4/
+# License https://creativecommons.org/licenses/by-sa/3.0/
+hump <- png::readPNG('./img/PhyloPic.ce70490a.Chris-huh.Balaenoptera-novaeangliae.png', native = T)
+
+# Beluga whale - Delphinapterus leucas
+# by Xavier Giroux-Bougard
+# http://phylopic.org/image/f1367ab1-40cf-4e9a-a84b-37508f11a7c7/
+bew <- png::readPNG('./img/PhyloPic.f1367ab1.Xavier-Giroux-Bougard.Delphinapterus_Delphinapterus-leucas_Monodontidae.png', native = T)
+
+# Atlantic cod - Gadus morhua
+# Milton Tan
+# http://phylopic.org/image/bba1800a-dd86-451d-a79b-c5944cfe5231/
+cod <- png::readPNG('./img/PhyloPic.bba1800a.Milton-Tan.Gadariae_Gadidae_Gadiformes_Gadinae_Gadus_Gadus-morhua_Zeiogadaria.png', native = T)
+
+# Capelin - Mallotus villosus
+# by xgirouxb
+# http://phylopic.org/image/f1f91d08-b850-4600-ad64-622ce87f0199/
+cap <- png::readPNG('./img/PhyloPic.f1f91d08.xgirouxb.Osmeridae_Osmeriformes_Osmerinae_Osmerini_Osmeroidea_Osmeroidei_Thaleichthys_Thaleichthys-pacificus.png', native = T)
+
+# Krill - Meganyctiphanes norvegica
+# by Steven Haddock • Jellywatch.org
+# http://phylopic.org/image/44a3628d-aafd-45cc-97a6-1cb74bd43dec/
+kri <- png::readPNG('./img/PhyloPic.44a3628d.Steven-Haddock-Jellywatch-org.Copepoda-Malacostraca_Eucarida_Eumalacostraca_Euphausiacea_Euphausiidae_Malacostraca.png', native = T)
+
+# Copepoda
+# by Joanna Wolfe
+# http://phylopic.org/image/c5dbd85a-c4be-4990-a369-c830ad23cb22/
+cop <- png::readPNG('./img/PhyloPic.c5dbd85a.Joanna-Wolfe.Calanoida_Copepoda_Epacteriscidae_Erebonectes_Gymnoplea_Neocopepoda.png', native = T)
+cop[cop == 16777216] <-0
+
+# Dynamics
+source('./Code/Figures/FR2-DynamicsCode.R')
+
+# ----------------------------------------------------------------------
+# Drivers
+# ----------------------------------------------------------------------
+SST <- png::readPNG('./img/SST.png', native = T) # modified from https://fontawesome.com/icons/thermometer-full?style=solid
+Shipping <- png::readPNG('./img/ship-solid.png', native = T) # https://fontawesome.com/icons/ship?style=solid
+DD <- png::readPNG('./img/trawl.png', native = T)
+
+
+# Colors
+colP <- c('#a86262cc','#a84d4d44')
+colB <- c('#097373cc','#196f6f44')
+colM <- c('#c2851acc','#db9f3844')
+# vars
+cx_title <- 1.1
+lwdD <- .75
+
+
+
+
+
+png('./Figures/FR2-Pathways.png', res = 900, width = 200, height = 120, units = "mm")
+# Layout
+# mat <- matrix(c(9:11,1,1,2,4,6,3,4,6,8,5,7,8,5,7,0), nrow = 3, ncol = 6)
+mat <- matrix(0, nrow = 3, ncol = 9)
+mat[1,] <- c(9,1,1,1,1,4,4,5,5)
+mat[2,] <- c(10,1,1,1,1,6,6,7,7)
+mat[3,] <- c(11,2,2,2,3,3,8,8,0)
+# layout(mat, widths = c(1,1,.5,.5,.5,.5))
+layout(mat, widths = c(1,.25,.25,.25,.25,.5,.5,.5,.5))
+# layout.show(11)
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Food web
+# ------------------
+# Plot 1
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+par(mar = c(2,.5,2,.5), family = 'serif')
+plot0(x = c(-.7, .7), y = c(-.7, 1.5))
+# Emphasis
+polygon(x = c(-.175,-.175,.7,.7), y = c(1.1,-.05,-.05,1.1), col = '#00000000', border = colsPos[3], lty = 2, lwd = lwdD)
+polygon(x = c(-.5,-.5,.5,.5), y = c(0.15,-.76,-.76,0.15), col = '#00000000', border = colsPos[4], lty = 2, lwd = lwdD)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Food web
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Interactions
+lines(x = c(0, 0), y = c(.15,.85)) # bel to cap
+lines(x = c(-.1, -.4), y = c(.1,.4)) # hump to cap
+lines(x = c(.1, .4), y = c(.1,.52)) # cod to cap
+lines(x = c(.1, .4), y = c(.9325,.7275)) # bel to cod
+lines(x = c(.045, .2375), y = c(-.1,-.52)) # cap to cop
+lines(x = c(-.045, -.2375), y = c(-.1,-.52)) # cap to kri
+lines(x = c(-.4725, -.325), y = c(.36,-.52)) # hump to kri
+
+# Pathways of effect
+# lines(x = c(-.4,-.31), y = c(1.3,-.52), col = colP[2], lty = 4) # SST to kri
+lines(x = c(-.4, .25), y = c(1.3,-.52), col = '#00000066', lty = 4) # SST to cop
+lines(x = c(0, 0), y = c(1.3,1.1), col = '#00000066', lty = 4) # SHP to bew
+lines(x = c(0, -.41), y = c(1.3,.6), col = '#00000066', lty = 4) # SHP to hump
+lines(x = c(.4, .485), y = c(1.3,.75), col = '#00000066', lty = 4) # FISH to cod
+lines(x = c(.4, .03), y = c(1.3,.1), col = '#00000066', lty = 4) # FISH to cap
+lines(x = c(.4, -.41), y = c(1.3,.6), col = '#00000066', lty = 4) # FISH to hump
+
+# Species
+cxy <- .6
+pchImage(x = -.51, .5, obj = hump, cex.x = 2.2, cex.y = cxy+.1, col = '#000000') #colsPos[1])
+pchImage(x = .01, 1, obj = bew, cex.x = 2, cex.y = cxy, col = '#000000') #colsPos[1])
+pchImage(x = .5, .66, obj = cod, cex.x = 1.55, cex.y = cxy, col = '#000000') #colsPos[1])
+pchImage(x = 0, .04, obj = cap, cex.x = 1.25, cex.y = cxy, col = '#000000') #colsPos[1])
+pchImage(x = -.3, -.66, obj = kri, cex.x = .85, cex.y = cxy-.1, col = '#000000') #colsPos[1])
+pchImage(x = .3, -.66, obj = cop, cex.x = .7, cex.y = cxy, col = '#000000') #colsPos[1])
+
+
+# Drivers
+pchImage(x = -.4, 1.45, obj = SST, cex.x = .8, cex.y = .65, col = '#00000099')
+pchImage(x = 0, 1.45, obj = Shipping, cex.x = .8, cex.y = .6, col = '#00000099')
+pchImage(x = .4, 1.45, obj = DD, cex.x = .8, cex.y = .6, col = '#00000099')
+
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Legends
+# ------------------
+# Plot 2 & 3
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Apparent competition
+# --------------------
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+
+# Points
+pt <- data.frame(x = c(-.4,.4,0), y = c(-1,-1,1))
+
+# Lines
+cn <- '#000000'
+# l <- data.frame(x1 = c(-.4,.4), x2 = c(0,0),
+#                 y1 = c(-1,-1), y2 = c(1,1),
+#                 arrow = c(1,2),
+#                 col = c(cn,cn))
+
+
+l <- data.frame(x1 = c(-.35,-.42,.42,.35), x2 = c(-.05,-.12,.12,.05),
+                y1 = c(-.8,-.8,-.8,-.8), y2 = c(.8,.8,.8,.8),
+                arrow = c(2,1,1,2),
+                col = c(cn,cn,cn,colsPos[4]))
+
+
+txt <- data.frame(txt = c('$e_{\\textit{xz}}$','$\\alpha_{\\textit{xz}}$',
+                          '$\\alpha_{\\textit{yz}}$','$e_{\\textit{yz}}$'),
+                  x = (l$x1+l$x2)/2,
+                  y = (l$y1+l$y2)/2,
+                  adjX = c(.2,.8,.2,.8),
+                  adjY = c(.5,.5,.5,.5),
+                  col = c(cn,cn,cn,colsPos[4]),
+                  stringsAsFactors = F)
+
+txt2  <- data.frame(txt = c('$\\textit{r_x}$','$\\textit{r_y}','\\textit{m_z}'),
+                    x = c(pt$x[1], pt$x[2], pt$x[3]),
+                    y = c(pt$y[1]-.25, pt$y[2]-.25, pt$y[3]+.25),
+                    col = c(cn,cn,colsPos[4]))
+
+# Plotq
+par(mar = c(0,1,1,1), family = 'serif')
+plot0(x = c(-1,.6), y = c(-2.05,1.2))
+text(x = c(-.25,-.25), y = c(-1.65,-1.95), adj = c(0,.5), cex = .75,
+     labels = TeX(c('Mortality rate ($\\textit{m}$)', 'Growth rate ($\\textit{r}$)')))
+
+# box(lty = 2, col = colsPos[3])
+# Title
+# text(x = -1, y = 1.6, labels = 'Pathways of effect', adj = c(0,.5))
+
+# Arrows
+for(i in 1:nrow(l)) arrows(l$x1[i], l$y1[i], l$x2[i], l$y2[i], length = .05, code = l$arrow[i], xpd = TRUE, lwd = 1, col = l$col[i])
+
+# Positions
+points(x = pt$x, y = pt$y, cex = 2, col = c(cn,colsPos[4],colsPos[4]), bg = '#bdbdbd', pch = 21, lwd = 1.25)
+
+# Text
+points(x = txt$x, y = txt$y, col = '#ffffff', bg = '#ffffff', pch = 21, lwd = 7)
+text(x = pt$x, y = pt$y, labels = c('x','y','z'), adj = c(.5,.35), font = 4, cex = .85)
+for(i in 1:nrow(txt)) text(x = txt$x[i], y = txt$y[i], labels = TeX(txt$txt[i]), adj = c(txt$adjX[i], txt$adjY[i]), cex = .75, col = txt$col[i])
+text(x = txt2$x, y = txt2$y, labels = TeX(txt2$txt), adj = c(.5,.5), cex = .75, col = txt2$col)
+
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Omnivory
+# --------------------
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Points
+pt <- data.frame(x = c(.35,-.35,.35),
+                 y = c(-1,.3,1))
+
+# Lines
+cn <- '#000000'
+l <- data.frame(x1 = c(-.24,  -0.285,   0.225, 0.26,  0.32,  0.38),
+                x2 = c(.285,   0.2325, -0.26, -0.23,  0.32,  0.38),
+                y1 = c(.15,    0.1,     .925,  0.86,  0.8,  0.8),
+                y2 = c(-.825, -0.875,   0.44,  0.37, -0.8, -0.8),
+                arrow = c(1,2,2,1,1,2),
+                col = c(cn,cn,colsPos[3],cn,cn,colsPos[3]))
+
+txt <- data.frame(txt = c('$e_{\\textit{xy}}$', '$\\alpha_{\\textit{xy}}$',
+                          '$\\alpha_{\\textit{yz}}$', '$e_{\\textit{yz}}$',
+                          '$e_{\\textit{xz}}$', '$\\alpha_{\\textit{xz}}$'),
+                  x = (l$x1+l$x2)/2,
+                  y = (l$y1+l$y2)/2,
+                  adjX = c(.35,.65,.65,.2,.8,.2),
+                  adjY = c(.4,.6,.3,.85,.5,.5),
+                  col = c(cn,cn,colsPos[3],cn,cn,colsPos[3]),
+                  stringsAsFactors = F)
+
+txt2  <- data.frame(txt = c('$\\textit{r_x}$','$\\textit{m_y}','\\textit{m_z}'),
+                    x = c(pt$x[1], pt$x[2]-.175, pt$x[3]),
+                    y = c(pt$y[1]-.25, pt$y[2], pt$y[3]+.25),
+                    col = c(colsPos[3],colsPos[3],cn))
+
+# Plot
+par(mar = c(0,1,1,1), family = 'serif')
+plot0(x = c(-.6,1), y = c(-2.05,1.2))
+# box(lty = 2, col = colsPos[3])
+text(x = c(-.5,-.5), y = c(-1.65,-1.95), adj = c(0,.5), cex = .75,
+     labels = TeX(c('Attack rate ($\\alpha$)','Conversion rate ($e$)')))
+
+
+# Arrows
+for(i in 1:nrow(l)) arrows(l$x1[i], l$y1[i], l$x2[i], l$y2[i], length = .05, code = l$arrow[i], xpd = TRUE, lwd = 1, col = l$col[i])
+
+# Positions
+points(x = pt$x, y = pt$y, cex = 2, col = colsPos[3], bg = '#bdbdbd', pch = 21, lwd = 1.25)
+
+# Text
+points(x = txt$x, y = txt$y, col = '#ffffff', bg = '#ffffff', pch = 21, lwd = 7)
+text(x = pt$x, y = pt$y, labels = c('x','y','z'), adj = c(.5,.35), font = 4, cex = .85)
+for(i in 1:nrow(txt)) text(x = txt$x[i], y = txt$y[i], labels = TeX(txt$txt[i]), adj = c(txt$adjX[i], txt$adjY[i]), cex = .75, col = txt$col[i])
+text(x = txt2$x, y = txt2$y, labels = TeX(txt2$txt), adj = c(.5,.5), cex = .75, col = txt2$col)
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Omnivory dynamics
+# ------------------
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Unitary 1
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+par(mar = c(.5, .5, 0, 0))
+plotOmnivory()
+# box2(side = '234', which = 'figure', lty = 2, col = colP[1], lwd = lwdD)
+
+# Delta abundances
+lines(x = c(5,55), y = rep(init['x'],2), lty = 4, col = '#000000aa', lwd = lwdD)
+xI <- init['x']
+xE <- res1[50,'x']
+xM <- mean(c(xI,xE))
+arrows(55, xM+(xI-xM)*.35, 55, xI-8.5, length = .025, code = 2, xpd = TRUE, lwd = lwdD)
+arrows(55, xM-(xI-xM)*.35, 55, xE+8.5, length = .025, code = 2, xpd = TRUE, lwd = lwdD)
+text(x = 55, y = xM, adj = c(.5,.5), labels = TeX('Sensitivity'), cex = .55)
+# text(x = 55, y = xM, adj = c(.5,.5), labels = TeX('$\\Delta a$'), cex = .55)
+
+# Population dynamics
+for (i in 2:4) lines(x = c(5:55), y = res1[,i], lwd = lwdD)
+
+# Disturbances
+# arrows(8, 550, 5.5, 520, length = .025, code = 2, xpd = TRUE)
+# pchImage(x = 10, xI+60, obj = DD, cex.x = .4, cex.y = 1)#, col = colB[1])
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Unitary 2
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Plot
+par(mar = c(.5, .5, 0, 0))
+plotOmnivory()
+for (i in 2:4) lines(x = c(5:55), y = res2[,i], lwd = lwdD)
+
+# Disturbances
+# pchImage(x = 10, 335, obj = DD, cex.x = .4, cex.y = 1)#, col = colB[1])
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Unitary 3
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Plot
+par(mar = c(.5, .5, 0, 0))
+plotOmnivory()
+for (i in 2:4) lines(x = c(5:55), y = res3[,i], lwd = lwdD)
+
+
+# Pathway of effect
+# pchImage(x = 10, 230, obj = Shipping, cex.x = .4, cex.y = 1)#, col = colB[1])
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Unitary 4
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Plot
+par(mar = c(.5, .5, 0, 0))
+plotOmnivory()
+for (i in 2:4) lines(x = c(5:55), y = res4[,i], lwd = lwdD)
+
+# Pathway of effect
+# pchImage(x = 10, 230, obj = Shipping, cex.x = .4, cex.y = 1)#, col = colB[1])
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Integrative
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Plot
+par(mar = c(.5, .5, 0, 0))
+plotOmnivory()
+
+# Pathways of effect
+# pchImage(x = 10, xI+60, obj = DD, cex.x = .4, cex.y = 1)#, col = colB[1])
+# pchImage(x = 10, 60, obj = Shipping, cex.x = .4, cex.y = 1)#, col = colB[1])
+
+# Legends
+## Pathways of effect
+lines(x = c(51,55), y = rep(xI+45,2), lwd = lwdD)
+lines(x = c(51,55), y = rep(xI-20,2), lty = 3, lwd = lwdD)
+text(x = 50, y = xI+50, adj = c(1,.5), labels = TeX('Integrative pathway of effect ($\\textit{G}$)'), cex = .55)
+text(x = 50, y = xI-15, adj = c(1,.5), labels = TeX('$\\sum$unitary pathways of effect ($\\textit{g}$)'), cex = .55)
+
+
+# Enveloppes
+envelop(x = 5:55, upper = init['x']-((init['x']-res1[,'x'])+(init['x']-res2[,'x'])+(init['x']-res3[,'x'])+(init['x']-res4[,'x'])), lower = res5[,'x'], col = '#00000022', border = '#00000000')
+envelop(x = 5:55, upper = init['y']-((init['y']-res1[,'y'])+(init['y']-res2[,'y'])+(init['y']-res3[,'y'])+(init['y']-res4[,'y'])), lower = res5[,'y'], col = '#00000022', border = '#00000000')
+envelop(x = 5:55, upper = init['z']-((init['z']-res1[,'z'])+(init['z']-res2[,'z'])+(init['z']-res3[,'z'])+(init['z']-res4[,'z'])), lower = res5[,'z'], col = '#00000022', border = '#00000000')
+
+# Joint model
+lines(x = c(5:55), y = res5[,'x'], lwd = lwdD)
+lines(x = c(5:55), y = res5[,'y'], lwd = lwdD)
+lines(x = c(5:55), y = res5[,'z'], lwd = lwdD)
+
+# Additive model
+lines(x = c(5:55), y = init['x']-((init['x']-res1[,'x'])+(init['x']-res2[,'x'])+(init['x']-res3[,'x'])+(init['x']-res4[,'x'])), lwd = .4, lty = 3)
+lines(x = c(5:55), y = init['y']-((init['y']-res1[,'y'])+(init['y']-res2[,'y'])+(init['y']-res3[,'y'])+(init['y']-res4[,'y'])), lwd = .4, lty = 3)
+lines(x = c(5:55), y = init['z']-((init['z']-res1[,'z'])+(init['z']-res2[,'z'])+(init['z']-res3[,'z'])+(init['z']-res4[,'z'])), lwd = .4, lty = 3)
+
+# Non-additive
+xI <- init['x']-((init['x']-res1[,'x'])+(init['x']-res2[,'x'])+(init['x']-res3[,'x'])+(init['x']-res4[,'x']))[50]
+xE <- res5[50,'x']
+xM <- mean(c(xI,xE))
+text(x = 54, y = xM, labels = 'Amplification', cex = .55, adj = c(1,.5))
+# text(x = 54, y = xM, labels = 'Synergy', cex = .55, adj = c(1,.5))
+
+
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Omnivory dynamics
+# ------------------
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Unitary 1
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+par(mar = c(.5, .5, 0, 0))
+plotApparent()
+for (i in 2:4) lines(x = c(5:55), y = ap1[,i], lwd = lwdD)
+
+# Delta abundances
+lines(x = c(5,55), y = rep(apInit['z'],2), lty = 4, col = '#000000aa', lwd = lwdD)
+xI <- apInit['z']
+xE <- ap1[50,'z']
+xM <- mean(c(xI,xE))
+arrows(55, xM+(xI-xM)*.35, 55, xI-8.5, length = .025, code = 2, xpd = TRUE, lwd = lwdD)
+arrows(55, xM-(xI-xM)*.35, 55, xE+8.5, length = .025, code = 2, xpd = TRUE, lwd = lwdD)
+text(x = 55, y = xM, adj = c(.5,.5), labels = TeX('Sensitivity'), cex = .55)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Unitary 2
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Plot
+par(mar = c(.5, .5, 0, 0))
+plotApparent()
+for (i in 2:4) lines(x = c(5:55), y = ap2[,i], lwd = lwdD)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Integrative
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Plot
+par(mar = c(.5, .5, 0, 0))
+plotApparent()
+for (i in 2:4) lines(x = c(5:55), y = ap3[,i], lwd = lwdD)
+
+# Legends
+## Pathways of effect
+lines(x = c(51,55), y = rep(xI+175,2), lwd = lwdD)
+lines(x = c(51,55), y = rep(xI+50,2), lty = 3, lwd = lwdD)
+text(x = 50, y = xI+175, adj = c(1,.5), labels = TeX('Integrative pathway of effect ($\\textit{G}$)'), cex = .55)
+text(x = 50, y = xI+50, adj = c(1,.5), labels = TeX('$\\sum$unitary pathways of effect ($\\textit{g}$)'), cex = .55)
+
+
+# Enveloppes
+envelop(x = 5:55, upper = apInit['x']-((apInit['x']-ap1[,'x'])+(apInit['x']-ap2[,'x'])), lower = ap3[,'x'], col = '#00000022', border = '#00000000')
+envelop(x = 5:55, upper = apInit['y']-((apInit['y']-ap1[,'y'])+(apInit['y']-ap2[,'y'])), lower = ap3[,'y'], col = '#00000022', border = '#00000000')
+envelop(x = 5:55, upper = apInit['z']-((apInit['z']-ap1[,'z'])+(apInit['z']-ap2[,'z'])), lower = ap3[,'z'], col = '#00000022', border = '#00000000')
+
+# Joint model
+lines(x = c(5:55), y = ap3[,'x'], lwd = lwdD)
+lines(x = c(5:55), y = ap3[,'y'], lwd = lwdD)
+lines(x = c(5:55), y = ap3[,'z'], lwd = lwdD)
+
+# Additive model
+lines(x = c(5:55), y = apInit['x']-((apInit['x']-ap1[,'x'])+(apInit['x']-ap2[,'x'])), lwd = .4, lty = 3)
+lines(x = c(5:55), y = apInit['y']-((apInit['y']-ap1[,'y'])+(apInit['y']-ap2[,'y'])), lwd = .4, lty = 3)
+lines(x = c(5:55), y = apInit['z']-((apInit['z']-ap1[,'z'])+(apInit['z']-ap2[,'z'])), lwd = .4, lty = 3)
+
+# Non-additive
+xI <- apInit['z']-((apInit['z']-ap1[,'z'])+(apInit['z']-ap2[,'z']))[50]
+xE <- ap3[50,'z']
+xM <- mean(c(xI,xE))
+text(x = 54, y = xM, labels = 'Amplification', cex = .55, adj = c(1,.5))
+
+dev.off()
